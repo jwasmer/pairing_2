b69f49d6330c6af809d6679f7b7755a98960bc49
James Wasmer <jameswasmer@gmail.com>
2022-08-03 18:20:41 -0400

    Add friends.js exercise

---
+++ friends.js
@@ -0,0 +1,9 @@
+
+var bestFriendName = "Tim"
+
+if (bestFriendName === "Tim") {
+  console.log("You're my best friend.")
+}
+else if (bestFriendName !== "Tim"){
+  console.log("You're not my best friend.")
+}
